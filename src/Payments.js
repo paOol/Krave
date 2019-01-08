f9367f9d0f2be66d9d0a155e775342f3606f642d
Unknown <paoolkim@gmail.com>
2019-01-08 02:17:57 -0800

    remove bitbox

--- src/Payments.js
+++ src/Payments.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 import localStorage from 'store';
 import { QRCode } from 'react-qr-svg';
 import uniqid from 'uniqid';
+import openSocket from 'socket.io-client';
 
+const socketio = openSocket('http://localhost:4646');
 const BITBOXSDK = require('bitbox-sdk/lib/bitbox-sdk').default;
 const BITBOX = new BITBOXSDK();
 var socket;
@@ -13,11 +15,10 @@ class Payments extends React.Component {
   state = {
     uniqid: '',
     depositAddress: '',
-    amount: '',
     transactionResponse: '',
+    socketResponse: '',
     error: ''
   };
-
   assignUniqid = () => {
     let rand = uniqid();
     localStorage.set('user', { id: rand });
@@ -28,6 +29,12 @@ class Payments extends React.Component {
     return axios
       .post(`api/address`, { uniqid: uniqid })
       .then(x => {
+        let obj = {
+          ...this.props.data,
+          depositAddress: x.data,
+          uniqid: uniqid
+        };
+        socketio.emit('depositAddress', obj);
         return this.setState({
           depositAddress: x.data
         });
@@ -47,58 +54,13 @@ class Payments extends React.Component {
       }
       this.generateAddress(uniqid);
 
-      socket = new BITBOX.Socket({
-        restURL: 'https://datnode.com'
-      });
-      socket.listen('transactions', message => {
-        this.handleNewTx(message);
+      socketio.on('bcash', x => {
+        this.setState({
+          socketResponse: x
+        });
       });
     }
   }
-
-  handleNewTx = msg => {
-    const { amount, depositAddress, uniqid } = this.state;
-    const json = JSON.parse(msg);
-    const outputs = json.outputs;
-    let txid = json.format.txid;
-
-    const addresses = this.getOutputAddresses(outputs);
-
-    addresses.forEach(a => {
-      let key = Object.keys(a)[0];
-
-      if (key !== undefined) {
-        let cashAddr = BITBOX.Address.toCashAddress(key);
-        if (cashAddr === depositAddress) {
-          if (a[key].value <= 0.0000008) {
-            this.setState({
-              error: 'amount received was less than 0.008 BCH'
-            });
-          } else {
-            this.setState({
-              amount: a[key].value,
-              transactionResponse: txid
-            });
-            this.props.paymentReceived(uniqid, txid);
-          }
-        }
-      }
-    });
-  };
-
-  getOutputAddresses = outputs => {
-    const addresses = outputs.reduce((prev, curr, idx) => {
-      const txid = curr.scriptPubKey.hex;
-      const addressArray = curr.scriptPubKey.addresses;
-      const value = BITBOX.BitcoinCash.toBitcoinCash(curr.satoshi);
-      const ret = addressArray.reduce((prev, curr, idx) => {
-        return { ...prev, [curr]: { value } };
-      }, {});
-      return [...prev, { ...ret }];
-    }, []);
-
-    return addresses;
-  };
 
   payWithBadger = amount => {
     let { web4bch, depositAddress, uniqid } = this.state;
@@ -116,26 +78,21 @@ class Payments extends React.Component {
           console.log(';err', err);
           return;
         }
-        this.setState({
-          amount: amount,
-          transactionResponse: txid
-        });
-
-        this.props.paymentReceived(uniqid, txid);
       });
     }
   };
 
   render() {
-    const { depositAddress, error, transactionResponse } = this.state;
-    const { number, username } = this.props;
+    const { depositAddress, error, socketResponse } = this.state;
+    const { number, username, paymentReceived } = this.props;
 
     return (
       <div className="wrapper centered">
-        {transactionResponse ? (
+        {socketResponse ? (
           <Response
             amount={this.state.amount}
-            transactionResponse={transactionResponse}
+            socketResponse={socketResponse}
+            paymentReceived={paymentReceived}
           />
         ) : (
           <div className="qr">
@@ -184,28 +141,24 @@ class Response extends React.Component {
   refresh = () => {
     window.location.reload();
   };
-  toSatoshis(n) {
-    var result = n - Math.floor(n) !== 0;
-    if (result) {
-      return n * 100000000;
-    }
-    return n;
-  }
-  render() {
-    let { amount, transactionResponse } = this.props;
-    let newamount = this.toSatoshis(amount);
-    let newcost = this.toSatoshis(cost);
 
-    if (newamount >= newcost) {
+  render() {
+    let { socketResponse } = this.props;
+
+    if (socketResponse.success) {
+      this.props.paymentReceived();
       return (
         <div className="success">
           <h2>
-            success! <br />
+            {socketResponse.status}
+            <br />
             you can view the tx &nbsp;
             <a
               target="_blank"
               rel="nofollow"
-              href={`https://blockchair.com/bitcoin-cash/transaction/${transactionResponse}`}
+              href={`https://blockchair.com/bitcoin-cash/transaction/${
+                socketResponse.txid
+              }`}
             >
               here
             </a>
@@ -215,12 +168,12 @@ class Response extends React.Component {
           </h4>
         </div>
       );
-    } else if (newamount < newcost) {
+    } else {
       return (
         <div className="error">
           <h2>
             Error! <br />
-            The amount sent was under 0.008 BCH.
+            {socketResponse.status}
           </h2>
           <h4>
             Click <span onClick={this.refresh}> here</span> to try again.
--- src/Upcoming.js
+++ src/Upcoming.js
@@ -1,25 +1,21 @@
 import React from 'react';
 
 class Upcoming extends React.Component {
-  state = {
-    blockheight: ''
-  };
-
-  componentDidMount() {}
-
   render() {
     const { jobs } = this.props;
+
     return (
       <div className="wrapper centered">
         <h2>Upcoming Registrations</h2>
         <div className="container">
-          {jobs.map((x, i) => {
-            return (
-              <div className="account" key={i}>
-                {`${x.username}#${x.number}`}
-              </div>
-            );
-          })}
+          {jobs &&
+            jobs.map((x, i) => {
+              return (
+                <div className="account" key={i}>
+                  {`${x.username}#${x.number}`}
+                </div>
+              );
+            })}
         </div>
       </div>
     );
